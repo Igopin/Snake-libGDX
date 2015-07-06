0eb20cecfdad4843a5ec1b05c5658f527ae45dc0
Igor Pinaev <igopin@gmail.com>
2015-07-06 04:50:25 +0300

    initial commit

---
+++ core/src/com/igopin/spacesnake/SpaceSnake.java
@@ -0,0 +1,12 @@
+package com.igopin.spacesnake;
+
+import com.badlogic.gdx.Game;
+
+public class SpaceSnake extends Game {
+
+    @Override
+    public void create() {
+
+    }
+
+}
---
+++ desktop/src/com/igopin/spacesnake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.igopin.spacesnake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.igopin.spacesnake.SpaceSnake;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new SpaceSnake(), config);
+    }
+}
